4100337f9e761fbe1a310355c2c7ffce6d239307
Casey Morford <casey.morford@purplewave.com>
2016-05-08 21:19:58 -0500

    Initial Commit

---
+++ php/recipes/configure.rb
@@ -0,0 +1,109 @@
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping php::configure application #{application} as it is not an PHP app")
+    next
+  end
+
+  # write out opsworks.php
+  template "#{deploy[:deploy_to]}/shared/config/opsworks.php" do
+    cookbook 'php'
+    source 'opsworks.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :database => deploy[:database],
+      :memcached => deploy[:memcached],
+      :layers => node[:opsworks][:layers],
+      :stack_name => node[:opsworks][:stack][:name]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+  # write out opsworks.php
+  template "#{deploy[:deploy_to]}/shared/config/database.php" do
+    cookbook 'php'
+    source 'database.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+        :db_host => node[:deploy][:ci_config][:db_host],
+        :db_username => node[:deploy][:ci_config][:db_username],
+        :db_password => node[:deploy][:ci_config][:db_password],
+        :db_name => node[:deploy][:ci_config][:db_name],
+        :ro_db_host => node[:deploy][:ci_config][:ro_db_host],
+        :ro_db_username => node[:deploy][:ci_config][:ro_db_username],
+        :ro_db_password => node[:deploy][:ci_config][:ro_db_password],
+        :ro_db_name => node[:deploy][:ci_config][:ro_db_name]
+    )
+  end
+
+    # write out opsworks.php
+  template "#{deploy[:deploy_to]}/shared/config/config.php" do
+    cookbook 'php'
+    source 'config.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :encryption_key => node[:deploy][:ci_config][:encryption_key],
+      :lb_hostname => node[:deploy][:ci_config][:lb_hostname],
+      :lb_protocol => node[:deploy][:ci_config][:lb_protocol],
+      :rodb_on => node[:deploy][:ci_config][:rodb_on], 
+      :ruby_api_url => node[:deploy][:ci_config][:ruby_api_url]
+    )
+    
+  end
+
+        # write out php.ini
+  template "/etc/php.ini" do
+    cookbook 'php'
+    source 'php.ini.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :timezone => node[:deploy][:ci_config][:timezone],
+      :memcache_cluster_uri => node[:deploy][:ci_config][:memcache_cluster_uri]
+    )
+  end
+
+
+
+  directory "#{deploy[:deploy_to]}/current/application/logs/" do
+    recursive true
+    mode 0775
+    owner "deploy"
+    group "apache"
+    action :create
+  end
+
+  execute "set_timezone" do
+    command 'sudo mv /etc/localtime /etc/localtime.bak; sudo ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime'
+  end
+
+  service "httpd" do
+    action :restart
+  end
+
+  service "awslogs" do
+    action :restart
+  end
+
+  template "#{deploy[:deploy_to]}/current/index.php" do
+    cookbook 'php'
+    source 'index.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+
+    variables(
+        :environment => node[:deploy][:ci_config][:environment]
+        )
+  end
+
+  
+end
---
+++ rails/recipes/configure.rb
@@ -0,0 +1,61 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
+  node.default[:deploy][application][:database][:adapter] = OpsWorks::RailsConfiguration.determine_database_adapter(application, node[:deploy][application], "#{node[:deploy][application][:deploy_to]}/current", :force => node[:force_database_adapter_detection])
+  deploy = node[:deploy][application]
+
+  case node[:deploy][application][:database][:adapter]
+  when /mysql/
+    include_recipe "mysql::client_install"
+  when "postgresql"
+    include_recipe "opsworks_postgresql::client_install"
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+    source "database.yml.erb"
+    cookbook 'rails'
+    mode "0660"
+    group 'root'
+    owner 'root'
+    variables(
+        deploy[:host] => node[:deploy][:ci_config][:db_host], 
+        deploy[:username] => node[:deploy][:ci_config][:db_username],
+        deploy[:password] => node[:deploy][:ci_config][:db_password],
+        deploy[:database] => node[:deploy][:ci_config][:db_name]
+         )
+    puts " ----------------------------------------------------------------------------------------- "
+    puts deploy[:host].inspect
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      deploy[:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
+  # template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
+  #   source "memcached.yml.erb"
+  #   cookbook 'rails'
+  #   mode "0660"
+  #   group deploy[:group]
+  #   owner deploy[:user]
+  #   variables(
+  #     :memcached => deploy[:memcached] || {},
+  #     :environment => deploy[:rails_env]
+  #   )
+
+  #   notifies :run, "execute[restart Rails app #{application}]"
+
+  #   only_if do
+  #     deploy[:memcached][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+  #   end
+  # end
+end
